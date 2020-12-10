21e1e423bfe92546ccfbb02fa4ac6b76de1b7b17
zizimoos_iMac <zizimoos@gmail.com>
2020-12-10 23:59:19 +0900

    Router and @types done!

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,21 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+
+export const isLoggedInVar = makeVar(false);
 
 export const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          isLoggedIn: {
+            read() {
+              return isLoggedInVar();
+              // return Boolean(localStorage.getItem("token"));
+            },
+          },
+        },
+      },
+    },
+  }),
 });
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,3 +1,14 @@
 import React from "react";
+import { isLoggedInVar } from "../apollo";
 
-export const LoggedInRouter = () => <span>Logged In</span>;
+export const LoggedInRouter = () => {
+  const onClick = () => {
+    isLoggedInVar(false);
+  };
+  return (
+    <div>
+      <h1>Logged In</h1>
+      <button onClick={onClick}>Log Out</button>
+    </div>
+  );
+};
