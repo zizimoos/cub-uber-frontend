1f37b059317ca82d4a8376bb0d4f7f8046440baf
zizimoos_iMac <zizimoos@gmail.com>
2020-12-25 13:40:55 +0900

    ....

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,7 +1,22 @@
-describe("First Test", () => {
-  it("should go to homepage", () => {
-    cy.visit("http://localhost:3000")
-      .title()
-      .should("eq", "Login | Cub Uber Eat");
+describe("Log In", () => {
+  it("should see login page", () => {
+    cy.visit("/").title().should("eq", "Login | Cub Uber Eat");
+  });
+  it("can fill out the form", () => {
+    cy.visit("/")
+      .get('[name="email"]')
+      .type("blackpinkrisa@risa.com")
+      .get('[name="password"]')
+      .type("1234567890")
+      .get(".text-white")
+      .should("not.have.class", "pointer-event-none");
+    // to do (can log in)
+  });
+  it("can see email / password validation errors", () => {
+    cy.visit("/")
+      .get('[name="email"]')
+      .type("bad@email")
+      .get(".text-red-500")
+      .should("have.text", "Please enter a vaild email");
   });
 });
